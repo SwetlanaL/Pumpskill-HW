254dfa38d138560bbf0d5385dfe0554300dae6eb
Svetlana Leovochkina <sleovochkina@gmail.com>
2020-10-05 10:36:43 +0300

    add other solutions after webinar

--- fibonacci.py
+++ fibonacci.py
@@ -1,22 +1,47 @@
-from functools import lru_cache
+# from functools import lru_cache
 
-@lru_cache(maxsize = 10000)
-def fibonacci(n):
-    if n == 1:
-        return 1
-    elif n == 2:
-        return 2
-    elif n > 2:
-        return (fibonacci(n-1) + fibonacci(n-2))
+# @lru_cache(maxsize = 10000)
+# def fibonacci(n):
+#     if n == 1:
+#         return 1
+#     elif n == 2:
+#         return 2
+#     elif n > 2:
+#         return (fibonacci(n-1) + fibonacci(n-2))
     
-n = 1
-even_numbers = []
-while fibonacci(n) < 10000000:
-    if fibonacci(n) % 2 == 0:
-        even_numbers.append(fibonacci(n))
-        sum_even_numbers = sum(even_numbers)
-    n += 1
-print(f'Количество элементов в последовательности: {n}')
-print(f'Сумма всех четных элементов: {sum_even_numbers}')
-print(f'Четные элементы последовательности: {even_numbers}')
-print(f'{fibonacci(n-2)}  - предпоследнее число последовательности')
+# n = 1
+# even_numbers = []
+# while fibonacci(n) < 10000000:
+#     if fibonacci(n) % 2 == 0:
+#         even_numbers.append(fibonacci(n))
+#         sum_even_numbers = sum(even_numbers)
+#     n += 1
+# print(f'Количество элементов в последовательности: {n}')
+# print(f'Сумма всех четных элементов: {sum_even_numbers}')
+# print(f'Четные элементы последовательности: {even_numbers}')
+# print(f'{fibonacci(n-2)}  - предпоследнее число последовательности')
+
+#OR (через создание списка из чисел фибоначчи):
+# threshold - порог
+def fibonacci(zero_step, first_step, threshold):
+    next_step = 0
+    list_of_numbers = [zero_step, first_step]
+    while next_step < threshold:
+        next_step = zero_step + first_step
+        zero_step = first_step
+        first_step = next_step
+        if next_step < threshold:
+            list_of_numbers.append(next_step)
+    return list_of_numbers
+fibonacci_list = fibonacci(1, 1, 10000000)
+
+even_list = []
+for number in fibonacci_list:
+    if number % 2 == 0:
+        even_list.append(number)
+
+print(f'Количество элементов в последовательности: {len(fibonacci_list)}')
+print(f'Сумма всех четных элементов: {sum(even_list)}')
+print(f'Четные элементы последовательности: {even_list}')
+print(f'{fibonacci_list[- 2]}  - предпоследнее число последовательности')
+
--- plural_form.py
+++ plural_form.py
@@ -1,13 +1,35 @@
-def plural_form(num, *args):
-    first_digit = num % 10
-    second_digit = int(num / 10) % 10
-    if second_digit != 1:
-        if first_digit == 1:
-            return str(num) + ' ' + args[0]
-        elif first_digit >= 2 and first_digit <= 4:
-            return str(num)+ ' ' + args[1]
-    return str(num)+ ' ' + args[2]
+# def plural_form(num, *args):
+#     first_digit = num % 10
+#     second_digit = int(num / 10) % 10
+#     if second_digit != 1:
+#         if first_digit == 1:
+#             return str(num) + ' ' + args[0]
+#         elif first_digit >= 2 and first_digit <= 4:
+#             return str(num)+ ' ' + args[1]
+#     return str(num)+ ' ' + args[2]
             
+# print(plural_form(1, 'яблоко', 'яблока', 'яблок'))
+# print(plural_form(2, 'яблоко', 'яблока', 'яблок')) 
+# print(plural_form(11, 'студент', 'студента', 'студентов')) 
+# print(plural_form(15, 'студент', 'студента', 'студентов')) 
+# print(plural_form(3, 'студент', 'студента', 'студентов')) 
+
+#OR:
+
+def plural_form(value, form1, form2, form3):
+    #abs() - возвращает положительное число
+    n = abs(value) % 100
+    n1 = n % 10
+    result_form = form3
+    if 10 < n < 20 or n == 0:
+        result_form = form3
+    elif  1 < n1 < 5:
+        result_form = form2
+    elif n1 == 1:
+        result_form = form1
+    return f'{value} {result_form}'
+
+print(plural_form(0, 'яблоко', 'яблока', 'яблок'))
 print(plural_form(1, 'яблоко', 'яблока', 'яблок'))
 print(plural_form(2, 'яблоко', 'яблока', 'яблок')) 
 print(plural_form(11, 'студент', 'студента', 'студентов')) 
